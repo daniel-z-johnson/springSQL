45f24e60fcbb0beab322dfc6b1dbb2c61b597ae7
daniel-z-johnson <danzaljohnson@gmail.com>
2020-11-17 01:43:29 -0700

    initial commit

---
+++ src/test/java/self/practice/sql/article/ArticleApplicationTests.java
@@ -0,0 +1,13 @@
+package self.practice.sql.article;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
